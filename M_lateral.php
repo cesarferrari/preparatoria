bd90403adbbced928016848d2f398d37e12e1593
unknown <cesarferrari1000@gmail.com>
2023-05-05 13:06:18 -0500

    menu_lateral

---
+++ M_lateral.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latral</title>
+    <link rel="stylesheet" href="css_lateral.css">
+
+
+    <header>
+
+    </header>
+
+    <div class="contenedora">
+        <section class="menu">
+
+        </section>
+        <div class="contenido">
+
+        </div>
+    </div>
+
+</head>
+<body>
+    
+</body>
+</html>
